ec20e3b8547e9eea7f4f326cae49f7450ba94331
Mbvanvl <mbvanvl@gmail.com>
2025-02-06 03:30:55 -0800

    Test

--- index.js
+++ index.js
@@ -62,9 +62,9 @@ client.on(Events.MessageCreate, async message => {
 
   if (DEBUG)
     console.log(`User ${message.author.id} sent a message in channel ${message.channelId} at ${message.createdTimestamp}`)
-  
+
   if (message.author.id != '176532282935345153' && !user.roles.cache.has(ADMINFLAG))
-      DBHandler.insertMessage(getTimestamp(), message.author.id, 1);
+    DBHandler.insertMessage(getTimestamp(), message.author.id, 1);
 
   if (message.content.startsWith('!activity')) {
 
@@ -77,101 +77,101 @@ client.on(Events.MessageCreate, async message => {
         filterNum = 5;
     }
 
-  const messages = await DBHandler.getMessages();
-  const voice = await DBHandler.getVoiceStatus();
-  let Users = {};
-  for (let i = 0; i < messages.length; i++) {
-    if (Date.parse(messages[i][0]) < Date.now() - cutOffTime)
-      continue;
-    if (Users[messages[i][1]]) {
-      Users[messages[i][1]][2] += messages[i][2];
-    } else {
-      Users[messages[i][1]] = [...messages[i], []];
-    }
-  }
-  for (let i = 0; i < voice.length; i++) {
-    if (Date.parse(voice[i][0]) < Date.now() - cutOffTime)
-      continue;
-    if (Users[voice[i][1]]) {
-      Users[voice[i][1]][3].push([voice[i][0], voice[i][1], voice[i][2]]);
-    } else {
-      Users[voice[i][1]] = [0, voice[1][1], 0, [[voice[i][0], voice[i][1], voice[i][2]]]];
-    }
-  }
-
-
-  for (const key in Users) {
-    let totalTime = 0;
-    let lastJoinTime = 0;
-
-    for (let i = 0; i < Users[key][3].length; i++) {
-      if (Users[key][3][i][2] === 'JOIN') {
-        lastJoinTime = Date.parse(Users[key][3][i][0]);
-      } else if (Users[key][3][i][2] === 'LEAVE') {
-        if (lastJoinTime === 0)
-          continue;
-        totalTime += Date.parse(Users[key][3][i][0]) - lastJoinTime;
-        lastJoinTime = 0;
-      }
-    }
-    if (lastJoinTime !== 0)
-      totalTime += Date.parse(getTimestamp()) - lastJoinTime;
-
-    Users[key][3] = `${(totalTime)}`;
-  }
-
-  let ara = [['Username', 'Messages', 'Voice Time', 'Activity']];
-
-  for (const key in Users) {
-    const tUser = message.guild.members.cache.get(Users[key][1]);
-    ara.push([tUser.user.username, Users[key][2], convertToTime(Users[key][3]), ((Users[key][2] + Math.floor((Users[key][3] / (1000 * 60 * 5)))))]);
-  }
-
-  ara = ara.sort(function (a, b) {
-    return b.Activity - a.Activity;
-  });
-
-  ara = ara.slice(0, filterNum + 1);
-
-  let colLengths = [8, 8, 10, 8]
-  let headerLengths = [8, 8, 10, 8]
-
-  for (let i = 1; i < ara.length; i++) {
-    for (let j = 0; j < ara[i].length; j++) {
-      colLengths[j] = Math.max(colLengths[j], String(ara[i][j]).length);
-    }
-  }
-  let str = '```md\n'
-
-  for (let i = 0; i < ara.length; i++) {
-
-    if (i !== 0) {
-      str += '> '
-    } else {
-      str += '  ';
-    }
-    for (let j = 0; j < ara[i].length; j++) {
-      if (i === 0) {
-        str += String(ara[i][j]).padEnd(colLengths[j] + 4, ' ');
+    const messages = await DBHandler.getMessages();
+    const voice = await DBHandler.getVoiceStatus();
+    let Users = {};
+    for (let i = 0; i < messages.length; i++) {
+      if (Date.parse(messages[i][0]) < Date.now() - cutOffTime)
+        continue;
+      if (Users[messages[i][1]]) {
+        Users[messages[i][1]][2] += messages[i][2];
       } else {
-        if (j !== 0)
-          str += String(ara[i][j]).padEnd(colLengths[j] + 4, ' ');
-        else
-          str += String(ara[i][j]).padEnd(colLengths[j] + 4, ' ');
+        Users[messages[i][1]] = [...messages[i], []];
       }
     }
-    if (i === 0) {
-      str += '\n';
-      str += '-'.repeat(colLengths.reduce((a, b) => a + b, 0) + 14);
+    for (let i = 0; i < voice.length; i++) {
+      if (Date.parse(voice[i][0]) < Date.now() - cutOffTime)
+        continue;
+      if (Users[voice[i][1]]) {
+        Users[voice[i][1]][3].push([voice[i][0], voice[i][1], voice[i][2]]);
+      } else {
+        Users[voice[i][1]] = [0, voice[1][1], 0, [[voice[i][0], voice[i][1], voice[i][2]]]];
+      }
     }
-    str += '\n';
+
+
+    for (const key in Users) {
+      let totalTime = 0;
+      let lastJoinTime = 0;
+
+      for (let i = 0; i < Users[key][3].length; i++) {
+        if (Users[key][3][i][2] === 'JOIN') {
+          lastJoinTime = Date.parse(Users[key][3][i][0]);
+        } else if (Users[key][3][i][2] === 'LEAVE') {
+          if (lastJoinTime === 0)
+            continue;
+          totalTime += Date.parse(Users[key][3][i][0]) - lastJoinTime;
+          lastJoinTime = 0;
+        }
+      }
+      if (lastJoinTime !== 0)
+        totalTime += Date.parse(getTimestamp()) - lastJoinTime;
+
+      Users[key][3] = `${(totalTime)}`;
+    }
+
+    let ara = [['Username', 'Messages', 'Voice Time', 'Activity']];
+
+    for (const key in Users) {
+      const tUser = message.guild.members.cache.get(Users[key][1]);
+      ara.push([tUser.user.username, Users[key][2], convertToTime(Users[key][3]), ((Users[key][2] + Math.floor((Users[key][3] / (1000 * 60 * 5)))))]);
+    }
+
+    ara = ara.sort(function (a, b) {
+      return b.Activity - a.Activity;
+    });
+
+    ara = ara.slice(0, filterNum + 1);
+
+    let colLengths = [8, 8, 10, 8]
+    let headerLengths = [8, 8, 10, 8]
+
+    for (let i = 1; i < ara.length; i++) {
+      for (let j = 0; j < ara[i].length; j++) {
+        colLengths[j] = Math.max(colLengths[j], String(ara[i][j]).length);
+      }
+    }
+    let str = '```md\n'
+
+    for (let i = 0; i < ara.length; i++) {
+
+      if (i !== 0) {
+        str += '> '
+      } else {
+        str += '  ';
+      }
+      for (let j = 0; j < ara[i].length; j++) {
+        if (i === 0) {
+          str += String(ara[i][j]).padEnd(colLengths[j] + 4, ' ');
+        } else {
+          if (j !== 0)
+            str += String(ara[i][j]).padEnd(colLengths[j] + 4, ' ');
+          else
+            str += String(ara[i][j]).padEnd(colLengths[j] + 4, ' ');
+        }
+      }
+      if (i === 0) {
+        str += '\n';
+        str += '-'.repeat(colLengths.reduce((a, b) => a + b, 0) + 14);
+      }
+      str += '\n';
+    }
+
+    str += '```'
+
+
+    message.reply({ content: str, ephemeral: true });
   }
-
-  str += '```'
-
-
-  message.reply({ content: str, ephemeral: true });
-}
 
 });
 
