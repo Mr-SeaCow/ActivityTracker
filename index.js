3f52d62b3625a06390c907da4935d604ad997463
Mbvanvl <mbvanvl@gmail.com>
2025-02-05 23:49:14 -0800

    Test

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ client.on(Events.MessageCreate, async message => {
       }
     }
     if (lastJoinTime !== 0)
-      totalTime += Date.parse(getTimestamp()) - lastJoinTime;
+      totalTime += lastJoinTime - Date.parse(getTimestamp());
 
     Users[key][3] = `${(totalTime)}`;
   }
