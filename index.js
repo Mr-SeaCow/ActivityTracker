ed0da45934a54fd0e9ecb2b24fdd170ed77ab657
Mbvanvl <mbvanvl@gmail.com>
2025-02-06 12:03:50 -0800

    test

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ client.on(Events.MessageCreate, async message => {
     for (const key in Users) {
       const tUser = message.guild.members.cache.get(Users[key][1]);
       if (tUser == undefined) {
-        tUser = client.guild.cache.get('1328455635431588044')
-        tUser = tUser.guild.members.cache.get(Users[key][1])
+        tUser = client.guilds.cache.get('1328455635431588044')
+        tUser = tUser.members.cache.get(Users[key][1])
       }
 
       ara.push([tUser.user.username, Users[key][2], convertToTime(Users[key][3]), ((Users[key][2] + Math.floor((Users[key][3] / (1000 * 60 * 5)))))]);
